33387603fdfb2d33405fed2c4a09c836cfd9e99c
Nirjala <bhattarainirjala22@gmail.com>
2026-09-15 20:57:17 +0545

    Account app update

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+
+def register(request):
+    if request.method== 'POST':
+        form=RegistrationForm(request.POST)
+
+        if form.is_valid():
+            User=form.save(commit=False)
+            User.set_password(form.cleaned_data['password'])
+            User.save()
+            return redirect('login')
+    else:  
+         form=RegistrationForm()
+    return render(request,'register.html',{'form':form})
+
 
 # Create your views here.
