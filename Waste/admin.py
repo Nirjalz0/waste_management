bbf2eae8b680f30428a6b858a947050b6771862c
Nirjala <bhattarainirjala22@gmail.com>
2026-09-15 20:57:37 +0545

    waste app update

--- Waste/admin.py
+++ Waste/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import wasteCategory
+from .models import wasteCategory , collectionRequest
 admin.site.register(wasteCategory)
+admin.site.register(collectionRequest)
 # Register your models here.
--- Waste/models.py
+++ Waste/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 class wasteCategory(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField(blank=True)
@@ -7,8 +8,33 @@ class wasteCategory(models.Model):
     def __str__(self):
         return self.name
 
+class collectionRequest(models.Model):
+    customer=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    waste_category=models.ForeignKey(
+        wasteCategory,
+        on_delete=models.CASCADE
+    )
+    quantity=models.FloatField()
+    address=models.TextField()
+    preferred_date=models.DateField()
 
+    STATUS_CHOICES=[
+        ('pending','pending'),
+        ('assigned','assigned'),
+        ('collected','collected'),
+        ('cancelled','cancelled'),
 
-    
+    ]
+    status=models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+
+    )
+
+    created_at=models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
